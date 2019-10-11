abc9d4d3d2c04f2a35bfb99c800b46480a886254
Adam Byrd <ajbyrd0723@gmail.com>
2019-10-11 15:42:56 -0500

    almost added the exclamations in the right spot

--- overly-excited.js
+++ overly-excited.js
@@ -10,11 +10,24 @@ function addExcitement (theWordArray) {
 
     // Each time the for loop executes, you're going to add one more word to this string
     let buildMeUp = ""
+    
+
 
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp = `${buildMeUp} ${theWordArray[i]}`   
+         ;
+        
+        if (i % 3 === 0) {
+         
+            buildMeUp = `${buildMeUp} ${theWordArray[i]}!` 
+        }
 
+        else {
+            buildMeUp = `${buildMeUp} ${theWordArray[i]}` 
+        }    
+    
+
+        
+        
         // Print buildMeUp to the console
         console.log(buildMeUp)
     }
